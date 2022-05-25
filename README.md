57956990fee9f791738202e6b9aa06772a5ac017
Elina <elina2412@yahoo.com>
2022-05-25 11:38:44 -0400

    init feature

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# week2_projectv0
+# week2_projectv0 
 
 # ------------------------------------------------------------------------
 # ReadMe 
@@ -13,12 +13,24 @@
 
     declare -a ARR=("sdfsdfsdfsfs" "45454545454" "454545sdfsdf45454" "454545454DDD54" "sdfsdfsdfsdfsA" "sdfs56A" "sdfsfs56A" "sdfssfs56A" "sdfss2fs56A")
 
+    ##if file do not exists, create it
+
+    if ! [ -f "./mypath/password.txt" ]; then
+        mkdir mypath
+        touch mypath/password.txt
+        chmod +x mypath/password.txt
+    fi
+
     ##now loop through the above array
 
     for pw in "${ARR[@]}"
         do
         bash password-validator.sh "$pw"
         echo -e "pw from text: exitCode=$? $pw\n"
+
+        echo "$pw" > ./mypath/password.txt
+        bash password-validator.sh -f "/mypath/password.txt"
+        echo -e "pw from file: exitCode=$? $pw\n"
 
         done
 
--- password-validator.sh
+++ password-validator.sh
@@ -7,6 +7,7 @@ RED="\033[31m"
 GREEN="\033[32m"
 declare -i EXIT_CODE=1
 INPUT=$@
+STR_TO_COMPARE="-f /"
 
 # ---------------------------------------------------------------------------------------------
 # pwValidation // checks validation of the password
@@ -37,9 +38,43 @@ fi
 
 }
 
-if [ -z "$INPUT" ]; then
-    echo -e $RED 'Invalid input. Please insert input in this format: {./password-validator.sh "MyPW123"}'
+# if [ -z "$INPUT" ]; then
+#     echo -e $RED 'Invalid input. Please insert input in this format: {./password-validator.sh "MyPW123"}'
 
+# else
+#     pwValidation "$INPUT"
+# fi
+
+
+# ---------------------------------------------------------------------------------------------
+# Check the option that user has been chosen: a. Password as an input. b. Password inside file
+# ---------------------------------------------------------------------------------------------
+
+checkInputCategory(){
+if [ -z "$INPUT" ]; then
+    echo -e $RED 'Invalid input. Please insert input in this format: {./password-validator.sh "MyPW123"} or {./password-validator.sh -f "/myPath/password.txt"}'
 else
-    pwValidation "$INPUT"
+    # If chosen option to check password in file.
+    if [ "${INPUT::4}" = "${STR_TO_COMPARE}" ]; then
+        FILE_PATH_AND_NAME="."${INPUT:3}
+        echo $FILE_PATH_AND_NAME
+        # Checks if file is exists
+        if [ -f "${FILE_PATH_AND_NAME}" ]; then
+            PW_FROM_FILE=$(<${FILE_PATH_AND_NAME})   
+            pwValidation "${PW_FROM_FILE}"
+        else
+            echo -e $RED "The file: $FILE_PATH_AND_NAME do not exists. Please enter valid file." 
+        fi
+    # If chosen option to check password as text.
+    else
+        pwValidation "$INPUT"
+    fi
 fi
+}
+
+# ---------------------------------------------------------------------------------------------
+# init
+# ---------------------------------------------------------------------------------------------
+
+checkInputCategory
+exit $EXIT_CODE
