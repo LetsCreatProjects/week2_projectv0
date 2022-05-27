06a262967e91b0965d5bd756a293792f1cad9f8e
Elina <elina2412@yahoo.com>
2022-05-26 22:07:35 -0400

    Found and fixed bug.

--- password-validator.ps1
+++ password-validator.ps1
@@ -6,20 +6,16 @@
 param($in)
 $global:LASTEXITCODE = 1
 
-# checks the user's input in command line
-$global:arr_cliHistory = @(Get-History)
-# takes last command
-$global:str_lastCliCommand = $arr_cliHistory[-1]
+# getting user input
+$global:var1 =$args[0]
+$global:var2 =$args[1]
 
-
-# $tmp = Get-History | select-last 1
-
- Write-Host $in
 #-----------------------
 # pwValidation // checks validation of the password
 # ----------------------
 function pwValidation {
     param ($pw)
+
     # --if greater than 10
     if ($pw.length -gt 9) {
         # -- if contains at least one number
@@ -46,25 +42,21 @@ function pwValidation {
 
 function checkInputCategory() {
     # --- If chosen option to check password in file.
-    if ($global:str_lastCliCommand -like '* -f "/*') {
+    if ($global:var1.Count -gt 0 -and $global:var1 -eq '-f'){
+        
+        $filePathAndName = $global:var2.Trim().TrimStart('/')
 
-        # retrieve filepath from input
-        $filePathAndName = "$global:str_lastCliCommand".replace('./password-validator.ps1 -f "', '')
-
-        # Delete last char if the value of it is: "
-        $filePathAndName = $filePathAndName.Trim().TrimEnd('"')
-        $filePathAndName = $filePathAndName.Trim().TrimStart('/')
-
-                if (Test-Path -Path "$filePathAndName") {
-                    $pw_from_file = [IO.File]::ReadAllText("$filePathAndName")
+        # Write-Host $args[0]
+        if (Test-Path -Path "$filePathAndName") {
+            $pw_from_file = [IO.File]::ReadAllText("$filePathAndName")
             
-                    if (!$pw_from_file) { Write-Host 'Please insert the password inside the file: (the password should contain minimum 10 characters, that contains number, small and capital letter.' -ForegroundColor Red ; $global:LASTEXITCODE = 1 } 
-                    else {
-                        $pw_from_file=cat $filePathAndName  
-                        pwValidation "${pw_from_file}"
-                    }
-                }
-                else { Write-Host "The file: $filePathAndName do not exists. Please enter valid file."  -ForegroundColor Red ; $global:LASTEXITCODE = 1 } 
+            if (!$pw_from_file) { Write-Host 'Please insert the password inside the file: (the password should contain minimum 10 characters, that contains number, small and capital letter.' -ForegroundColor Red ; $global:LASTEXITCODE = 1 } 
+            else {
+                $pw_from_file = cat $filePathAndName  
+                pwValidation "${pw_from_file}"
+            }
+        }
+        else { Write-Host "The file: $filePathAndName do not exists. Please enter valid file."  -ForegroundColor Red ; $global:LASTEXITCODE = 1 } 
     }
     # --- If chosen option to check password as text.
     elseif (!$in) { Write-Host 'Invalid input. Please insert input in this format: {./password-validator.ps1 "MyPW123"} or {./password-validator.ps1 -f "/mypath/password.txt"}' -ForegroundColor Red ; $global:LASTEXITCODE = 1 } 
